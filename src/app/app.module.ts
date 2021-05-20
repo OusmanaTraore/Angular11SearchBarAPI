6fe4a53e9d536ff7ad8b6d8c31b3b452cbcc965e
OusmanaTraore <traoreosman@yahooo.fr>
2021-05-20 04:15:11 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GaugeModule } from 'angular-gauge';
+import { FormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http'
+import{MatTabsModule} from "@angular/material/tabs";
+import{MatIconModule} from "@angular/material/icon";
+import{MatSelectModule} from "@angular/material/select";
+import{MatFormFieldModule} from "@angular/material/form-field";
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    NoopAnimationsModule,
+    FormsModule,
+    GaugeModule.forRoot(),
+    HttpClientModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
